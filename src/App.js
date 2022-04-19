ce2f71481d0acb28a16bf50ed8c9836e598a9474
Rian <riansouza0@gmail.com>
2022-04-19 01:49:39 -0300

    project finished

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
-import Home from "./components/pages/Home";
-import Contact from "./components/pages/Contact";
-import NewProject from "./components/pages/NewProject";
-import Projects from "./components/pages/Projects";
-
-import Container from "./components/pages/layout/Container";
-import Navbar from './components/pages/layout/Navbar'
-import Footer from './components/pages/layout/Footer'
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
+import Home from './components/pages/Home'
+import Projects from './components/pages/Projects'
+import NewProject from './components/pages/NewProject'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Project from './components/pages/Project'
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Container customClass='min-height'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/newproject' element={<NewProject />} />
-          <Route path='/projects' element={<Projects />} />
-        </Routes>
-      </Container>
-
-      <Footer/>
-    </>
-
-  );
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/company">
+            <Company />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/newproject">
+            <NewProject />
+          </Route>
+          <Route path="/project/:id">
+            <Project />
+          </Route>
+        </Container>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+import styles from './Contact.module.css'
 
 function Contact() {
-    return (
-        <div><h1>Contato</h1></div>
-    )
+  return (
+    <section className={styles.section}>
+      <h1>Contato</h1>
+      <h4>Você pode me encontrar no meu:</h4>
+      <h4><a href="https://github.com/idkrian" target={'_blank'}> <FaGithub /> GitHub </a></h4>
+      <h4><a href="https://www.linkedin.com/in/riansouza/" target={'_blank'}> <FaLinkedin /> LinkedIn </a></h4>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
